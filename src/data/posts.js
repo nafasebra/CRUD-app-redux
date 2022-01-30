2e99724745ddce185291e92566a409cfb5bf50b4
unknown <ebranafas@gmail.com>
2022-01-30 23:50:03 +0330

    add data/post.js file

---
+++ src/data/posts.js
@@ -0,0 +1,35 @@
+const posts = [
+    {
+        id: 1,
+        title: "post 1",
+        topics: ["post", "first post"],
+        description: "this is a post description"
+    },{
+        id: 2,
+        title: "post 2",
+        topics: ["post", "first post"],
+        description: "this is a post description"
+    },{
+        id: 3,
+        title: "post 3",
+        topics: ["post", "first post"],
+        description: "this is a post description"
+    },{
+        id: 4,
+        title: "post 4",
+        topics: ["post", "first post"],
+        description: "this is a post description"
+    },{
+        id: 5,
+        title: "post 5",
+        topics: ["post", "first post"],
+        description: "this is a post description"
+    },{
+        id: 6,
+        title: "post 6",
+        topics: ["post", "first post"],
+        description: "this is a post description"
+    },
+]
+
+export default posts;
