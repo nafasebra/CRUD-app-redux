a6b28e2b86eec50f04e301ebf13ea21175f64d57
unknown <ebranafas@gmail.com>
2022-02-05 13:07:26 +0330

    add hover button style

--- src/components/modal/AddPost.js
+++ src/components/modal/AddPost.js
@@ -7,12 +7,13 @@ const Addpost = () => {
     return (
         <div>
             <form action="">
+                <input type="file" />
                 <input type="text" placeholder='post title' className={theme.form_input} />
-                <input type="text" placeholder='post title' className={theme.form_input} />
-                <input type="text" placeholder='post title' className={theme.form_input} />
-                <input type="text" placeholder='post title' className={theme.form_input} />
-                <input type="text" placeholder='post title' className={theme.form_input} />
-                <input type="text" placeholder='post title' className={theme.form_input} />
+                <input type="text" placeholder='post topics' className={theme.form_input} />
+                <textarea placeholder='post description' className={theme.form_input} rows="10"></textarea>
+                <button className={theme.btn_blue}>
+                    <span>Create post</span>
+                </button>
             </form>
         </div>
     );
