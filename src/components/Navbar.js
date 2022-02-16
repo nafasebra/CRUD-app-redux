09bdefc8930980a04cd6f32edb540d4c3d43f22c
unknown <ebranafas@gmail.com>
2022-02-16 13:18:08 +0330

    use dispathc to navbar component for use openmodal action

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { OpenModal } from '../actions/modalAction';
+import { useDispatch } from 'react-redux';
+
 import style from './Navbar.module.css';
 import theme from './Theme.module.css';
 
+
 const Navbar = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <div className={style.navbar}>
             <div className={style.navbar_logo}>
@@ -16,7 +23,12 @@ const Navbar = () => {
                     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
                 </button>
 
-                <button className={theme.btn_icon}>
+                <button 
+                    className={theme.btn_icon}
+                    onClick={
+                        () => dispatch(OpenModal({hasTitle: true, textOfTitle: 'hello'}))
+                    }
+                >
                     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
                 </button>
             </div>
