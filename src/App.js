ba4511fea90974afa0b3fd77853a7850180683a3
unknown <ebranafas@gmail.com>
2022-02-20 00:29:01 +0330

    add localstorage for declare of theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Navbar from './components/Navbar';
 import CardSection from './components/card/CardSection';
 import ModalContainer from './container/ModalContainer';
 import Footer from './components/Footer';
 
+import { setDarkTheme, setLightTheme } from './actions/themeAction';
+
 import './App.css';
+import { useEffect } from 'react';
 
 
 function App() {
 
 	const theme = useSelector(state => state.themeReducer);
+	const dispatch = useDispatch();
+
+	const setTheme = (theme = 'dark') => {
+		if(theme === 'dark')
+			return dispatch(setDarkTheme())
+		else 
+			return dispatch(setLightTheme())
+	}
+
+	useEffect(() => {
+		if(!localStorage.getItem('theme')){
+			localStorage.setItem('theme', theme);
+		}
+		else{
+			const storedTheme = localStorage.getItem('theme');
+			setTheme(storedTheme);
+		}
+	}, []);
+
+	useEffect(() => {
+		if(localStorage.getItem('theme')){
+			localStorage.setItem('theme', theme);
+		}	
+	}, [theme])
+	
 
 	return (
 		<div className={theme === 'dark' ? 'app-dark' : 'app-light'}>
