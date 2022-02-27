d93edf8ea7b3e0f635a1f352f0a89cdf6d5b48f0
unknown <ebranafas@gmail.com>
2022-02-27 12:36:16 +0330

    handle create post

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,18 +5,17 @@ import './Popup.css';
 
 
 function Popup() {
-  const [showPopup, setShowPopup] = useState(false);
+  const [showPopup] = useState(false);
 
-  console.log(setShowPopup);
 
   return (
     <div className={showPopup ? "popup_container show" : "popup_container"}>
         <div className='popup_icon'>
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         </div>
         <p className='popup_message'>Post is deleted!</p>
         <button className='btn_icon'>
-          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+          <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg>
         </button>
     </div>
   )
--- src/components/modal/child/AddPost.js
+++ src/components/modal/child/AddPost.js
@@ -1,23 +1,77 @@
 import React from 'react';
+import { useState } from 'react';
+
 
 import '../../Theme.css';
+
+import { CreatePost } from '../../../actions/postAction'
+import { CloseModal } from '../../../actions/modalAction'
+import { useDispatch } from 'react-redux';
 
 
 const Addpost = () => {
 
+    const dispatch = useDispatch();
+
+    const [ entryPostInfos, setEnteryPostInfos ] = useState({
+        id: 0,
+        imageURL: 'gdfgdfg',
+        title: '',
+        topics: [],
+        description: ''
+    });
+
+    const changeImageURL = (e) => {
+        let value = e.target.files[0];
+        if(value){
+            value = value.name;
+            setEnteryPostInfos(prev => {
+                return {
+                    ...prev,
+                    imageURL: value
+                }
+            })
+        }
+    }
+
+    const submitForm = () => {
+        setEnteryPostInfos(prev => {
+            return {
+                ...prev,
+                id: 6
+            }
+        })
+        dispatch(CreatePost(entryPostInfos));
+        dispatch(CloseModal());
+    }
+
+
     return (
         <div className="text_light">
             <label htmlFor="uploadImagePost" className="imageupload_label">
-                <input type="file" id="uploadImagePost" />
+                <input 
+                    type="file" 
+                    id="uploadImagePost" 
+                    onChange={(e) => changeImageURL(e)}    
+                />
                 <div className="imageupload_section">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                    <p>click to upload file</p>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
+                    <p>
+                        {entryPostInfos.imageURL ? entryPostInfos.imageURL : 'click to upload file'}
+                    </p>
                 </div>
             </label>
+
             <input type="text" placeholder='post title' className="form_input" />
+            
             <input type="text" placeholder='post topics' className="form_input" />
+            
             <textarea placeholder='post description' className="form_input" rows="10"></textarea>
-            <button className="btn_blue">
+            
+            <button 
+                className="btn_blue"
+                onClick={() => submitForm()}
+            >
                 <span>Create post</span>
             </button>
         </div>
