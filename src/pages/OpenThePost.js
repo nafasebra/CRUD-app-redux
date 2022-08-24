ffb21f86d9edc98fd787f418d4b17e1e80c07370
unknown <ebranafas@gmail.com>
2022-08-24 14:04:12 +0430

    add style for blog page

--- src/pages/OpenThePost.js
+++ src/pages/OpenThePost.js
@@ -1,20 +1,91 @@
-import React from 'react'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import "../components/card/Card.css";
 
 function OpenThePost() {
   return (
-    <div className=''>
-      <h1>
-         Lorem ipsum dolor sit amet.
-      </h1>
-      <img src="..." alt="the Lorem ipsum dolor sit amet" />
-      <p>
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque? Voluptates, laudantium dignissimos praesentium, unde magni eum accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt. Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus suscipit.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque? Voluptates, laudantium dignissimos praesentium, unde magni eum accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt. Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus suscipit.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque? Voluptates, laudantium dignissimos praesentium, unde magni eum accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt. Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus suscipit.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque? Voluptates, laudantium dignissimos praesentium, unde magni eum accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt. Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus suscipit.
-      </p>
-    </div>
-  )
+    <>
+      <Navbar />
+      <div className="blog_page">
+        <h1> Lorem ipsum dolor sit amet. </h1>
+        <img src="..." alt="the Lorem ipsum dolor sit amet" />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+          laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+          laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+          laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Assumenda laboriosam fugit doloremque sapiente excepturi nulla atque?
+          Voluptates, laudantium dignissimos praesentium, unde magni eum
+          accusantium aut blanditiis perspiciatis, quos iure ipsum deserunt.
+          Sunt doloribus, ab deleniti aut obcaecati praesentium temporibus
+          suscipit.
+        </p>
+
+        <div className="card_topic">nature</div>
+        <div className="card_topic">photo</div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default OpenThePost
+export default OpenThePost;
