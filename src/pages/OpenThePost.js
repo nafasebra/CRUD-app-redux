4861262a0661dc2fa32bdb3705f43e9e261982c2
unknown <ebranafas@gmail.com>
2022-09-11 12:49:20 +0430

    add image alternate when image src is empty (to openthepost page)

--- src/pages/OpenThePost.js
+++ src/pages/OpenThePost.js
@@ -21,10 +21,42 @@ function OpenThePost() {
       <Navbar showPlusBtn={false} />
       <div className="blog_page">
         <h1> {selectedData[0]?.title} </h1>
-        <img
-          src={selectedData[0]?.imageSrc}
-          alt="the Lorem ipsum dolor sit amet"
-        />
+
+        {selectedData[0]?.imageSrc !== "" ? (
+          <img
+            src={selectedData[0]?.imageSrc}
+            alt="the Lorem ipsum dolor sit amet"
+          />
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              padding: "7rem 0",
+              borderRadius: "20px",
+              color: "#ffffff",
+              backgroundColor: "#bbbbbb",
+              opacity: 0.3,
+            }}
+          >
+            <svg
+              style={{ width: "50px" }}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+          </div>
+        )}
         <p>{selectedData[0]?.description}</p>
 
         <div className="card_topic">nature</div>
